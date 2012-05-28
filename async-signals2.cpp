9c90bc4c75f156f56d135c0d454b1efcbc59ba0c
Anthony Foiani <anthony.foiani@gmail.com>
2012-05-27 23:30:11 -0600

    get rid of thread pool, switch to asio

--- async-signals2.cpp
+++ async-signals2.cpp
@@ -1,11 +1,3 @@
-/* compile line:
-
-   g++ -std=c++0x -Wall -pthread \
-     async-signals.cpp -o async-signals \
-     -lboost_thread-mt -lpthread -lrt
-
-*/
-
 #include <unistd.h>
 
 #include <iostream>
@@ -13,13 +5,15 @@
 #include <deque>
 #include <sstream>
 
+#include <boost/asio.hpp>
+#include <boost/lexical_cast.hpp>
 #include <boost/signals2.hpp>
-
+#include <boost/smart_ptr.hpp>
 #include <boost/thread.hpp>
 
 const int nap = 1;
 
-typedef boost::signals2::signal< void () > sig_t;
+typedef boost::signals2::signal< void () > sync_sig_t;
 
 // =====================================================================
 
@@ -79,132 +73,26 @@ operator << ( threaded_logger_t & tlt,
 
 // =====================================================================
 
-class thread_pool
-{
-public:
-    typedef boost::function< void () > work_t;
-
-    thread_pool( int n = 5 );
-    ~thread_pool();
-
-    void add_work( work_t work );
-
-private:
-
-    typedef boost::thread thread_t;
-    typedef std::vector< thread_t > thread_vec_t;
-
-    typedef std::deque< work_t > work_queue_t;
-
-    typedef boost::mutex mutex_t;
-    typedef boost::unique_lock< mutex_t > lock_t;
-    typedef boost::condition_variable cond_t;
-
-    bool running;
-
-    thread_vec_t thread_vec;
-
-    work_queue_t work_queue;
-    mutex_t work_mutex;
-    cond_t work_cond;
-
-    void do_work();
-};
-
-thread_pool::thread_pool( int n )
-    : running( true ),
-      thread_vec(),
-      work_queue(),
-      work_mutex(),
-      work_cond()
-{
-    for ( int i = 0; i < n; ++i )
-    {
-        thread_vec.push_back( thread_t( &thread_pool::do_work, this ) );
-        // tlog << "created thread " << thread_vec.back().get_id() << endl;
-    }
-}
-
-thread_pool::~thread_pool()
-{
-    {
-        lock_t work_lock( work_mutex );
-        running = false;
-    }
-
-    work_cond.notify_all();
-
-    for ( thread_t & t : thread_vec )
-    {
-        // boost::thread::id tid( t.get_id() );
-        // tlog << "joining thread " << tid << endl;
-        t.join();
-        // tlog << "joined thread " << tid << endl;
-    }
-}
-
-void
-thread_pool::add_work( work_t work )
-{
-    // tlog << "adding work " << endl;
-    lock_t work_lock( work_mutex );
-    work_queue.push_back( work );
-    work_cond.notify_one();
-}
-
-void
-thread_pool::do_work()
-{
-    while ( true )
-    {
-        work_t work;
-        {
-            lock_t work_lock( work_mutex );
-
-            if ( running )
-            {
-                // tlog << "waiting, running=" << running << endl;
-                work_cond.wait( work_lock );
-                // tlog << "woke up" << endl;
-            }
-
-            // tlog << "size=" << work_queue.size()
-            //      << ", running=" << running << endl;
-
-            if ( work_queue.empty() )
-            {
-                if ( running )
-                    continue;
-                else
-                    break;
-            }
-
-            work = work_queue.front();
-            work_queue.pop_front();
-        }
-
-        work();
-    }
-}
-
-typedef boost::shared_ptr< thread_pool > thread_pool_ptr;
-
 // =====================================================================
 
 struct async_combiner_t
 {
     typedef void result_type;
 
-    async_combiner_t() : pool_ptr( new thread_pool ) {}
+    async_combiner_t( boost::asio::io_service * io_ptr ) : io_ptr( io_ptr ) {}
     async_combiner_t( const async_combiner_t & ) = default;
 
-    thread_pool_ptr pool_ptr;
+    boost::asio::io_service * io_ptr;
 
     template < typename InputIter >
     void operator()( InputIter first, InputIter last )
     {
         for ( InputIter i = first; i != last; ++i )
-            pool_ptr->add_work( [=](){ *i; } );
+        {
+            tlog << "enqueuing" << endl;
+            io_ptr->post( [=](){ *i; } );
+            // tlog << "... done" << endl;
+        }
     }
 
 };
@@ -213,53 +101,91 @@ typedef boost::signals2::signal< void (), async_combiner_t > async_sig_t;
 
 // =====================================================================
 
-struct slot1_t
+struct slot_t
 {
-    void operator()()
-    {
-        tlog << "  slot1_t: start" << endl;
-        sleep( nap );
-        tlog << "  slot1_t: end" << endl;
-    }
-};
+    int n;
 
-struct slot2_t
-{
+    slot_t( int n ) : n( n ) {}
+
     void operator()()
     {
-        tlog << "  slot2_t: start" << endl;
+        tlog << "  slot " << n << ": start" << endl;
         sleep( nap );
-        tlog << "  slot2_t: end" << endl;
+        tlog << "  slot " << n << ": end" << endl;
     }
 };
 
 // =====================================================================
 
-int main( int /* argc */, char * /* argv */ [] )
+void
+run_io( boost::asio::io_service & io )
 {
-   slot1_t slot1;
-   slot2_t slot2;
+    tlog << "starting io_service.run" << endl;
+    io.run();
+    tlog << "ending io_service.run" << endl;
+}
 
-   tlog << "main: sync call, sync exec" << endl;
-   sig_t sig;
-   sig.connect( slot1 );
-   sig.connect( slot2 );
-   sig();
-   tlog << "main: done" << endl;
+int main( int argc, char * argv [] )
+{
+    slot_t slot1( 1 );
+    slot_t slot2( 2 );
 
-   tlog << "========================================" << endl;
-   tlog << "main: async call, sync exec" << endl;
-   boost::thread t1( [&](){ sig(); } );
-   tlog << "main: done" << endl;
-   t1.join();
+#if 0
+    {
+        tlog << "main: sync call, sync exec" << endl;
+        sync_sig_t sig;
+        sig.connect( slot1 );
+        sig.connect( slot2 );
+        sig();
+        tlog << "main: done" << endl;
+    }
 
-   tlog << "========================================" << endl;
-   tlog << "main: sync call, async exec" << endl;
-   async_sig_t async_sig;
-   async_sig.connect( slot1 );
-   async_sig.connect( slot2 );
-   async_sig();
-   tlog << "main: done" << endl;
+    tlog << "========================================" << endl;
 
-   return 0;
+    {
+        sync_sig_t sig;
+        sig.connect( slot1 );
+        sig.connect( slot2 );
+
+        tlog << "main: async call, sync exec" << endl;
+        boost::thread t1( [&](){ sig(); } );
+        tlog << "main: done" << endl;
+        t1.join();
+    }
+
+    tlog << "========================================" << endl;
+
+#endif
+
+    {
+        boost::asio::io_service io;
+
+        typedef boost::scoped_ptr< boost::asio::io_service::work > work_ptr_t;
+        work_ptr_t work_ptr( new boost::asio::io_service::work( io ) );
+
+        const int n_threads = argc > 1 ? boost::lexical_cast< int >( argv[1] ) : 5;
+        boost::thread_group threads;
+        for ( int i = 0; i < n_threads; ++i )
+        {
+            threads.create_thread( [&](){ run_io( io ); } );
+            // threads.create_thread( [&](){ io.run(); } );
+        }
+
+        async_combiner_t async_combiner( &io );
+        async_sig_t async_sig( async_combiner );
+        async_sig.connect( slot1 );
+        async_sig.connect( slot2 );
+
+        tlog << "main: sync call, async exec" << endl;
+        async_sig();
+        tlog << "main: done" << endl;
+        work_ptr.reset();
+
+        if ( n_threads )
+            threads.join_all();
+        else
+            run_io( io );
+    }
+
+    return 0;
 }
